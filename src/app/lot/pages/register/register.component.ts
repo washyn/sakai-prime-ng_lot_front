ddc22b92c5b4217ee40b34408f36d1b8aaa314ad
Washington <shin321654987@gmail.com>
2024-06-04 15:42:45 -0500

    aded register page

--- src/app/lot/pages/register/register.component.ts
+++ src/app/lot/pages/register/register.component.ts
@@ -1,9 +1,69 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import { Dictionary } from '@fullcalendar/core/internal';
+import {
+    CreateUpdateDocenteDto,
+    Gender,
+} from 'src/app/proxy/acme/book-store/entities';
+import {
+    DocenteService,
+    SelectService,
+} from 'src/app/proxy/acme/book-store/services';
+import { LookupDto } from 'src/app/proxy/washyn/unaj/lot';
 
 @Component({
     selector: 'app-register',
-    templateUrl:"./register.component.html",
+    templateUrl: './register.component.html',
     styleUrl: './register.component.css',
 })
-export class RegisterComponent { }
+export class RegisterComponent implements OnInit {
+    model: CreateUpdateDocenteDto;
+    formGroup: FormGroup;
+    grados: LookupDto<string>[] = [];
+
+    constructor(
+        public selectService: SelectService,
+        public formBuilder: FormBuilder,
+        public docenteService: DocenteService
+    ) {}
+    ngOnInit(): void {
+        this.selectService.getGrado().subscribe((res) => {
+            this.grados = res;
+        });
+        this.buildForm();
+    }
+
+    buildForm() {
+        this.formGroup = this.formBuilder.group<{
+            nombre: FormControl<string>;
+            apellidoPaterno: FormControl<string>;
+            apellidoMaterno: FormControl<string>;
+            gradoId: FormControl<string>;
+            genero: FormControl<Gender>;
+        }>({
+            nombre: new FormControl<string>('', [Validators.required]),
+            apellidoPaterno: new FormControl<string>('', [Validators.required]),
+            apellidoMaterno: new FormControl<string>('', [Validators.required]),
+            gradoId: new FormControl<string>('', [Validators.required]),
+            genero: new FormControl<Gender>(Gender.Unknow, [
+                Validators.required,
+            ]),
+        });
+    }
+
+    save() {
+        console.log(this.formGroup.value);
+
+        this.docenteService
+            .create({
+                ...this.formGroup.value,
+            } as CreateUpdateDocenteDto)
+            .subscribe(() => {});
+    }
+}
--- src/app/lot/pages/register/register.module.ts
+++ src/app/lot/pages/register/register.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DropdownModule } from 'primeng/dropdown';
+import { RadioButtonModule } from 'primeng/radiobutton';
 @NgModule({
     imports: [
         CommonModule,
-        RegisterRoutingModule
+        RegisterRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        InputTextModule,
+        ButtonModule,
+        AutoCompleteModule,
+        DropdownModule,
+        RadioButtonModule,
     ],
-    declarations: [RegisterComponent]
+    declarations: [RegisterComponent],
 })
-export class RegisterModule { }
+export class RegisterModule {}

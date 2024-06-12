1e1496ac25cade6604eadda114a187406290261d
Washington <shin321654987@gmail.com>
2024-06-12 12:07:30 -0500

    removed config component

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -14,7 +14,6 @@ import { AppMenuitemComponent } from './app.menuitem.component';
 import { RouterModule } from '@angular/router';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppFooterComponent } from './app.footer.component';
-import { AppConfigModule } from './config/config.module';
 import { AppSidebarComponent } from './app.sidebar.component';
 import { AppLayoutComponent } from './app.layout.component';
 import { ToastModule } from 'primeng/toast';
@@ -41,7 +40,6 @@ import { AbpUtilsModule } from '../lot/utils/abp-util.module';
         InputSwitchModule,
         RippleModule,
         RouterModule,
-        AppConfigModule,
         ToastModule,
         AbpUtilsModule,
     ],
